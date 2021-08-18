8bf74f9f8f279fb1c265e65bc59996ac3067e7e3
Aleksoft <aleksandrosilvinodeoliveiraa@gmail.com>
2021-08-18 19:26:15 -0300

    xy
    spring

---
+++ xy/src/test/java/Aleksoft/xy/XyApplicationTests.java
@@ -0,0 +1,13 @@
+package Aleksoft.xy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
